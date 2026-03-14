dbfcaf122660d7fb5195e22f0410606b06e29e09
Atom Mayilyan <mayilyan.atom@mail.ru>
2026-03-14 22:09:01 +0300

    Stored Procedures

---
+++ StoredProceduress/SQLQuery1-SelectDisciplineFromSchedule.sql
@@ -0,0 +1,23 @@
+--SQLQuery1-SelectDisciplineFromSchedule.sql
+USE PV_522_Import;
+GO
+
+-- sp_ -SelectSchedule
+CREATE PROCEDURE sp_SelectDisciplineFromSchedule  @discipline_name AS NVARCHAR(150)
+AS
+BEGIN
+	DECLARE @discipline AS SMALLINT = (SELECT discipline_id FROM Disciplines WHERE discipline_name LIKE @discipline_name)
+	SELECT
+			[Группа]	=	group_name,
+			[Дисциплина]	=	discipline_name,
+			[Пропадаватель]	=	FORMATMESSAGE(N'%s %s %s', last_name, first_name, middle_name),
+			[Дата]			=	[date],
+			[День недели]	=	DATENAME(WEEKDAY, [date]),
+			[Время]			=	[time],
+			[Статус]		=	IIF(spent=1,N'Преводено', N'запланировно')
+	FROM	Schedule, Groups, Disciplines,Teachers
+	WHERE	[group]			=	group_id
+	AND		discipline		=	discipline_id
+	AND		teacher			=	teacher_id
+	AND		discipline		=	@discipline
+END
